8214e0365595b5817171ac6db09d9d07cc7f4f79
LexRillo <fearsless@hotmail.it>
2023-03-29 22:08:20 +0100

    Improvement to Generalized Kuwahara filter
    Also added new interesting adaptation for super fast implementation by using separation on imaginary space

--- kuwaharaFilter.py
+++ kuwaharaFilter.py
@@ -3,6 +3,7 @@ from scipy.signal import convolve2d
 from skimage import io
 from skimage.color import rgba2rgb, rgb2hsv, hsv2rgb
 from skimage.filters import gaussian
+from skimage.util import img_as_ubyte
 from pathlib import Path
 import matplotlib.pyplot as plt
 
@@ -125,7 +126,7 @@ def original_Kuwahara_filter_colour(original_image, window_size : int):
 # TODO: Not fully working yet
 ## From the original paper "Artistic Edge and Corner Enhancing Smoothing" by Papari et al.
 ## https://www.cs.rug.nl/~imaging/artisticsmoothing/TIP_artistic.pdf
-def generalized_Kuwahara_filter(original_image, window_size : int):
+def generalized_Kuwahara_filter(original_image, window_size : int, sharpness : float = 0.5):
     ## Changes to the original Kuwahara implementation
     # change square kernel into circle kernel
     # change the four sectors into 8 sectors
@@ -139,40 +140,42 @@ def generalized_Kuwahara_filter(original_image, window_size : int):
         
     kernels = create_sector_kernels(window_size)
 
-    if original_image.shape[2] == 4:
-        img_copy = rgb2hsv(rgba2rgb(original_image))
-    else:
-        img_copy = rgb2hsv(original_image)
+    img_copy = original_image.astype(np.float64)
     
-    hue_component = img_copy[:, :, 0]
-    saturation_component = img_copy[:, :, 1]
-    brightness_component = img_copy[:, :, 2]
-    img_squared = brightness_component**2
+    img_squared = img_copy**2
     
-    hue_avg = np.zeros([img_copy.shape[0],img_copy.shape[1], 8])
-    saturation_avg = np.zeros([img_copy.shape[0],img_copy.shape[1], 8])
-    brightness_avgs = np.zeros([img_copy.shape[0],img_copy.shape[1], 8])
+    red_component = original_image[:,:,0]
+    green_component = original_image[:,:,1]
+    blue_component = original_image[:,:,2]
+    
+    red_component_squared = img_squared[:,:,0]
+    green_component_squared = img_squared[:,:,1]
+    blue_component_squared = img_squared[:,:,2]
+    
+    red_average = np.zeros([img_copy.shape[0],img_copy.shape[1], 8], dtype=float)
+    green_average = np.zeros([img_copy.shape[0],img_copy.shape[1], 8], dtype=float)
+    blue_average = np.zeros([img_copy.shape[0],img_copy.shape[1], 8], dtype=float)
     stddev_weights = np.zeros([img_copy.shape[0],img_copy.shape[1], 8], dtype=float)
     
     for k in range(8):
-        hue_avg[:, :, k] = convolve2d(hue_component, kernels[k],mode='same')
-        saturation_avg[:, :, k] = convolve2d(saturation_component, kernels[k],mode='same')
-        brightness_avgs[:, :, k] = convolve2d(brightness_component, kernels[k],mode='same')
-        stddev_weights[:, :, k] = convolve2d(img_squared, kernels[k],mode='same')
-        stddev_weights[:, :, k] = 1/(1 + np.sqrt(np.abs(stddev_weights[:, :, k]-brightness_avgs[:, :, k]**2)))
-    
-    io.imshow(np.sum(stddev_weights, axis = 2))
-    io.show()
-    hue_plane = np.sum(hue_avg * stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
-    saturation_plane = np.sum(saturation_avg * stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
-    brightness_plane = np.sum(brightness_avgs * stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
+        red_average[:, :, k] = convolve2d(red_component, kernels[k],mode='same')
+        green_average[:, :, k] = convolve2d(green_component, kernels[k],mode='same')
+        blue_average[:, :, k] = convolve2d(blue_component, kernels[k],mode='same')
+        red_squared_conv = convolve2d(red_component_squared, kernels[k],mode='same')
+        green_squared_conv = convolve2d(green_component_squared, kernels[k],mode='same')
+        blue_squared_conv = convolve2d(blue_component_squared, kernels[k],mode='same')
+        red_variance = np.abs(red_squared_conv - red_average[:, :, k]**2)
+        green_variance = np.abs(green_squared_conv - green_average[:, :, k]**2)
+        blue_variance = np.abs(blue_squared_conv - blue_average[:, :, k]**2)
+        # stddev_weights[:, :, k] = 1/(1 + np.sqrt(np.abs(stddev_weights[:, :, k]-brightness_avgs[:, :, k]**2)))
+        stddev_weights[:, :, k] = (1 + red_variance + green_variance + blue_variance)**(-sharpness*0.5)
     
     filtered = np.zeros(img_copy.shape)
-    filtered[:,:,0] = hue_plane
-    filtered[:,:,1] = saturation_plane
-    filtered[:,:,2] = brightness_plane
+    filtered[:,:,0] = np.sum(red_average *stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
+    filtered[:,:,1] = np.sum(green_average *stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
+    filtered[:,:,2] = np.sum(blue_average *stddev_weights, axis = 2)/np.sum(stddev_weights, axis = 2)
     
-    img_copy = hsv2rgb(filtered)
+    img_copy = img_as_ubyte(filtered/255)
     
     return img_copy
 
@@ -190,6 +193,11 @@ def anisotropic_Kuwahara_filter():
 def polynomial_anisotropic_Kuwahara_filter():
     pass
 
+
+# TODO: implement
+## From the Matlab implementation https://uk.mathworks.com/matlabcentral/fileexchange/58260-super-fast-kuwahara-image-filter-for-n-dimensional-real-or-complex-data
+def super_fast_Kuwahara_filter():
+    pass
 
 ### Kernels
 # Original Kawahara kernels
@@ -216,7 +224,7 @@ def gaussian_kernel(window_size : int, sigma = 2):
         raise Exception ("Invalid argument window size")
     k = np.zeros((window_size, window_size))
     
-    kernel_center = int((window_size+1)/2)
+    kernel_center = int((window_size)/2)
     k[kernel_center, kernel_center] = 1
     return gaussian(k, sigma=sigma)
 
@@ -235,9 +243,9 @@ def create_sector_kernels(window_size : int, sections : int = 8):
     
     original_gaussian_kernel = gaussian_kernel(window_size)
     
-    r = int((window_size+1)/2)
+    r = int((window_size)/2)
     for k in range(8):
-        section_kernel = np.fromfunction(lambda x, y: (is_point_in_circle(x, y, r))*(np.arctan2(x-r, y-r) + np.pi >= (((2*k))*np.pi/8)) * (np.arctan2(x-r, y-r)+np.pi <= (((2*k)+2)*np.pi/8)), (2*r+-1, 2*r-1), dtype=int).astype(np.uint8)
+        section_kernel = np.fromfunction(lambda x, y: (is_point_in_circle(x, y, r))*(np.arctan2(x-r, y-r) + np.pi >= (((2*k))*np.pi/8)) * (np.arctan2(x-r, y-r)+np.pi <= (((2*k)+2)*np.pi/8)), (2*r+1, 2*r+1), dtype=int).astype(np.uint8)
         section_kernel = section_kernel*original_gaussian_kernel
         section_kernel = section_kernel/np.sum(section_kernel)
         assert(np.isclose(np.sum(section_kernel), 1, rtol=1e-05, atol=1e-08, equal_nan=False))
@@ -259,19 +267,17 @@ if __name__ == '__main__':
     img = io.imread(image_path)
 
     # Apply Original Kuwahara filter
-    # og_kawa_gray = original_Kuwahara_filter_grayscale(img[:, :, 0], 9)
-    # og_kawa2 = naive_original_Kuwahara_filter_colour(img, 9)
-    # og_kawa3 = original_Kuwahara_filter_colour(img, 9)
+    og_kawa_gray = original_Kuwahara_filter_grayscale(img[:, :, 0], 9)
+    og_kawa2 = naive_original_Kuwahara_filter_colour(img, 9)
+    og_kawa3 = original_Kuwahara_filter_colour(img, 9)
 
     # Apply Generalized Kuwahara filter
     g_kawa = generalized_Kuwahara_filter(img, 9)
-    io.imshow(g_kawa)
-    io.show()
 
     # Apply Anisotropic Kuwahara filter
 
     # Show images
-    fig, axes = plt.subplots(2, 2, figsize=(7, 6))
+    fig, axes = plt.subplots(3, 2, figsize=(7, 6))
     ax = axes.ravel()
 
     ax[0].imshow(img, cmap=plt.cm.gray)
@@ -285,6 +291,10 @@ if __name__ == '__main__':
 
     ax[3].imshow(og_kawa3, cmap=plt.cm.gray)
     ax[3].set_title("Kawahara colour")
+    
+    ax[4].imshow(g_kawa, cmap=plt.cm.gray)
+    ax[4].set_title("Generalized Kawahara colour")
+    
     
     for a in ax.ravel():
         a.axis('off')
